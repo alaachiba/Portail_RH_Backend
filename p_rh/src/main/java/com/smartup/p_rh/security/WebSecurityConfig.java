b210435078ce5315f7a8977c7e518caf8ca06ed7
alaachiba <alaa.chiba@esprit.tn>
2021-03-13 14:38:54 +0100

    fixing signup 2

--- p_rh/src/main/java/com/smartup/p_rh/security/WebSecurityConfig.java
+++ p_rh/src/main/java/com/smartup/p_rh/security/WebSecurityConfig.java
@@ -64,7 +64,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-    
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/rest/api/**").authenticated().antMatchers("/v2/api-docs",
